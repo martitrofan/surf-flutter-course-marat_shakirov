90a8b229815bef55091f9e1bc4531bb8de832e31
martitrofan <pointpoint32@gmail.com>
2020-12-14 10:52:13 +0300

    task 4.3.1 - add Sight and mocks

---
+++ lib/domain/sigth.dart
@@ -0,0 +1,17 @@
+class Sight {
+  final String name;
+  final double lon;
+  final double lat;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight({
+    this.name,
+    this.lon,
+    this.lat,
+    this.url,
+    this.details,
+    this.type,
+  });
+}
